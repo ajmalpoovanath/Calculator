7f13548e59ca581998e70749164ba5fbd5f8ba8b
ajmalpoovanath <ajmal.poovanath@gmail.com>
2024-09-27 19:34:06 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ajmal Poovanath on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
